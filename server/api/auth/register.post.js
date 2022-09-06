f370bd1dde3a14eee0b939023cf7d98277054bd0
hsimao <e087754958@gmail.com>
2022-09-06 23:14:46 +0800

    feat: prisma setting with mongodb, basic user api

---
+++ server/api/auth/register.post.js
@@ -0,0 +1,43 @@
+import { sendError } from "h3";
+import { createUser } from "../../db/users";
+
+export default defineEventHandler(async (event) => {
+  const body = await useBody(event);
+  console.log("body", body);
+
+  const { name, username, email, password, repeatPassword } = body;
+
+  if (!name || !username || !email || !password || !repeatPassword) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 400,
+        statusMessage: "Invalid params"
+      })
+    );
+  }
+
+  if (password !== repeatPassword) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 400,
+        statusMessage: "Password do not match"
+      })
+    );
+  }
+
+  const userData = {
+    username,
+    email,
+    password,
+    name,
+    profileImage: "https:/picsum.photos/200/200"
+  };
+
+  const user = await createUser(userData);
+
+  return {
+    body: user
+  };
+});
