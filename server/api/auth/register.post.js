ea979ce9265e437507826b647426c7d8d9a578fe
hsimao <e087754958@gmail.com>
2022-09-07 20:02:28 +0800

    feat: register password 加密

--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -1,5 +1,6 @@
 import { sendError } from "h3";
 import { createUser } from "../../db/users";
+import { userTransformer } from "../../transformers/user";
 
 export default defineEventHandler(async (event) => {
   const body = await useBody(event);
@@ -38,6 +39,6 @@ export default defineEventHandler(async (event) => {
   const user = await createUser(userData);
 
   return {
-    body: user
+    body: userTransformer(user)
   };
 });
--- server/db/users.js
+++ server/db/users.js
@@ -1,7 +1,13 @@
 import { prisma } from "./index";
+import bcrypt from "bcrypt";
 
 export const createUser = (userData) => {
+  const finalUserData = {
+    ...userData,
+    password: bcrypt.hashSync(userData.password, 10)
+  };
+
   return prisma.user.create({
-    data: userData
+    data: finalUserData
   });
 };
