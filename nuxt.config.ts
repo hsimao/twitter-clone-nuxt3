9499a77a2a6bc58fa762461c608cbe7600bfb86f
hsimao <e087754958@gmail.com>
2022-09-15 11:27:19 +0800

    feat: edit nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,5 +12,8 @@ export default defineNuxtConfig({
     cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
     cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
     cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
+  },
+  build: {
+    transpile: ['@heroicons/vue']
   }
 })
