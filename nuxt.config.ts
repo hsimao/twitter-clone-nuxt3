39833bbcb439e4f9f6f71580579e12339fe1378d
hsimao <e087754958@gmail.com>
2023-01-07 16:19:54 +0800

    feat: ssg 手動、異步 routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+// import axios from 'axios'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
@@ -11,6 +13,27 @@ export default defineNuxtConfig({
     cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
     cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
   },
+  nitro: {
+    prerender: {
+      // 手動 route
+      routes: ['/tweet/1', '/tweet/2']
+    }
+  },
+  // 異步動態 routes
+  // hooks: {
+  //   async 'nitro:config'(config) {
+  //     const data = await axios
+  //       .get('xxxxx')
+  //       .catch((err) => {
+  //         console.log('api err', err)
+  //       })
+
+  //     const addRoutes = data.list.map(
+  //       (item) => `/tweet/${item.id}`
+  //     )
+  //     config.prerender.routes.push(...addRoutes)
+  //   }
+  // },
   build: {
     transpile: ['@heroicons/vue']
   },
